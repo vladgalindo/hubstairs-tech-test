f6014b96f238abf85132ed03eadfe058eca57575
Vladimir <vlad.galindo@gmail.com>
2020-01-11 16:24:19 -0500

    placing music library and playlist sections

--- src/components/music/index.js
+++ src/components/music/index.js
@@ -2,6 +2,18 @@ import React, {Fragment} from 'react';
 
 export const Music = () => (
     <Fragment>
-    <h1>Music</h1>
+        <h1 className="title">Music</h1>
+        <div className="columns">
+            <div className="column is-4">
+                <section id={'library'}>
+                    library
+                </section>
+            </div>
+            <div className="column is-4">
+                <section id={'playlist'}>
+                    playlist
+                </section>
+            </div>
+        </div>
     </Fragment>
 );
