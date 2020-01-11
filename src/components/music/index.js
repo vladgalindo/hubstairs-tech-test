d444cc3476a8dd4c34fd0cb4e926dc774dba6cad
Vladimir <vlad.galindo@gmail.com>
2020-01-11 16:37:32 -0500

    adding up songs, and loading the library

--- src/components/music/index.js
+++ src/components/music/index.js
@@ -1,19 +1,48 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import {library as musicLibrary} from '../../audio-files/library';
 
-export const Music = () => (
-    <Fragment>
-        <h1 className="title">Music</h1>
-        <div className="columns">
-            <div className="column is-4">
-                <section id={'library'}>
-                    library
-                </section>
+export const Music = () => {
+
+    const [library] = useState(musicLibrary);
+    const SONG_TITLE_CLASS = {marginRight: '10px'};
+
+    const renderLibrary = () => (
+
+        library.map(song => (
+            <Fragment key={song.title}>
+                <a className="panel-block">
+                            <span className="panel-icon">
+                              <i className="fas fa-music" aria-hidden="true"></i>
+                            </span>
+                    <span style={SONG_TITLE_CLASS}>{song.title}</span>
+                    <span className="panel-icon" >
+                              <i className="fas fa-play" aria-hidden="true"></i>
+                            </span>
+                </a>
+            </Fragment>
+        ))
+    );
+
+    return (
+        <Fragment>
+            <h1 className="title">Music</h1>
+            <div className="columns">
+                <div className="column is-4">
+                    <section id={'library'}>
+                        <nav className="panel">
+                            <p className="panel-heading">
+                                Library
+                            </p>
+                            {renderLibrary()}
+                        </nav>
+                    </section>
+                </div>
+                <div className="column is-4">
+                    <section id={'playlist'}>
+                        playlist
+                    </section>
+                </div>
             </div>
-            <div className="column is-4">
-                <section id={'playlist'}>
-                    playlist
-                </section>
-            </div>
-        </div>
-    </Fragment>
-);
+        </Fragment>
+    )
+};
