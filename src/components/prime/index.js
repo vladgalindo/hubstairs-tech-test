cb2dc175a74fd394d7a4f53b530784ce4cb4b367
Vladimir <vlad.galindo@gmail.com>
2020-01-11 17:16:33 -0500

    placing prime form and history containers

--- src/components/prime/index.js
+++ src/components/prime/index.js
@@ -2,6 +2,18 @@ import React, {Fragment} from 'react';
 
 export const Prime = () => (
     <Fragment>
-        <h1>Prime</h1>
+        <h1 className="title">Prime</h1>
+        <div className="columns">
+            <div className="column is-4">
+                <section>
+                    form...
+                </section>
+            </div>
+            <div className="column is-4">
+                <section id={'history'}>
+                    Prime query history
+                </section>
+            </div>
+        </div>
     </Fragment>
 );
