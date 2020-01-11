865149aa33de44f4e40340dfbaed6272da1ba23a
Vladimir <vlad.galindo@gmail.com>
2020-01-11 16:21:11 -0500

    wrapping up the dashboard

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,31 @@
 import React, {Fragment} from 'react';
+import {menuItems} from '../menu-items';
 
-export const Dashboard = () => (
-    <Fragment>
-        <h1>Dashboard</h1>
-    </Fragment>
-);
+export const Dashboard = (props) => {
+
+    const goToUrl = (url) => {
+        props.history.push(url);
+    }
+
+    return (
+        <Fragment>
+            {
+                menuItems.map(item => (
+                    <section key={item.label} className={`hero is-${item.color}`} onClick={() => {goToUrl(item.url)}}>
+                        <div className="hero-body">
+                            <div className="container">
+                                <h1 className="title">
+                                    {item.label}
+                                </h1>
+                                <h2 className="subtitle">
+                                    {item.info}
+                                </h2>
+                            </div>
+                        </div>
+                    </section>
+                ))
+            }
+
+        </Fragment>
+    )
+}
