54f31fac67ae3da651f70143efa58f0be475355d
Vladimir <vlad.galindo@gmail.com>
2020-01-11 17:41:00 -0500

    setting graph and form container

--- src/components/graph/index.js
+++ src/components/graph/index.js
@@ -2,6 +2,19 @@ import React, {Fragment} from 'react';
 
 export const Graph = () => (
     <Fragment>
-        <h1>Graph</h1>
+        <h1 className="title">Graph</h1>
+        <div className="columns">
+            <div className="column is-3">
+                Load an csv file to render the Chart...
+                <section>
+                    form...
+                </section>
+            </div>
+            <div className="column is-8">
+                <section id={'playlist'}>
+                    chart...
+                </section>
+            </div>
+        </div>
     </Fragment>
 );
